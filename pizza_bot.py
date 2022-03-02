a81b38678b9c53664a01ee8d408888d2e7e2b02b
Jacek Meres <jacekmeres@gmail.com>
2022-03-02 22:44:18 +0100

    add new file & pizza_bot.py -> IntEnum

--- pizza_bot.py
+++ pizza_bot.py
@@ -1,18 +1,22 @@
 # pizza bot - ordering
 
-print("""Available restaurants:
-    1: Mexican restaurant
-    2: Indian restaurant
-    3: Thai restaurant
-    4: Polish restaurant
-    5: Pizaa
-    6: Kebab
-    """)
-choose_res = input("Hello. What would you eat today? (please enter the number): ")
+from enum import IntEnum
 
-if choose_res == "1":
-    print("""Welcome in probably mexican restaurant.
-            Today we recommend awesome Burrito.
+
+Restaurants = IntEnum("Restaurants", ['Mexican', "Indian", "Thai", "Polish", "Pizza", "Kebab"])
+
+choose_res = int(input("""Available restaurants:
+    1. Mexican
+    2. Indian
+    3. Thai
+    4. Polish
+    5. Pizza
+    6. Kebab
+Hello. What would you eat today? (please enter the number): """))
+
+if choose_res == Restaurants.Mexican:
+    print("""   Welcome in probably mexican restaurant. 
+    Today we recommend awesome Burrito.
             """)
     burrito = input("Do you want to try? ").lower().split(' ')
     burrito_yes = ["yes", "please", "try", "yeah", "yep", "gladly"]
@@ -20,19 +24,6 @@ if choose_res == "1":
     if any(element in burrito_yes for element in burrito):
         print("Great choice. We are already preparing for you.")
     else:
-        other_choose = input("""So, how can I help you?
-                                This is our menu:
-                                    1. Nachos
-                                    2. Quesadillas
-                                    3. Chili con Carne
-                                    4. Sopa de Albondigas
-                                    5. Crema de Frijoles
-                                    6. Fajitas
-                                    7. Ensaladas
-                                    8. Brocheta de Cerdo
-                                    9. Enchiladas de Credo
-                                    10. Coronarita
-                                Can I take your order?(please enter the number): """)                  
         mex_menu = {'1': 'Nachos',
                     '2': 'Quesadillas',
                     '3': 'Chili con Carne',
@@ -44,25 +35,38 @@ if choose_res == "1":
                     '9': 'Enchiladas de Credo',
                     '10': 'Coronartia'}
 
-        if other_choose in mex_menu:
-            print("Good choice. Order accepted. We are already preparing your {}.".format(mex_menu[other_choose]))
+        choose_menu = input("""So, how can I help you?
+                                This is our menu:
+                                1. Nachos
+                                2. Quesadillas
+                                3. Chili con Carne
+                                4. Sopa de Albondigas
+                                5. Crema de Frijoles
+                                6. Fajitas
+                                7. Ensaladas
+                                8. Brocheta de Cerdo
+                                9. Enchiladas de Credo
+                                10.Coronarita
+                                Can I take your order?(please enter the number): """)
+        if choose_menu in mex_menu:
+            print("Good choice. Order accepted. We are already preparing your {}.".format(mex_menu[choose_menu]))
         else:
             print("Thanks...")
 
 
-elif choose_res == '2':
+elif choose_res == Restaurants.Indian:
     print("Menu in preparation.")
     
-elif choose_res == '3':
+elif choose_res == Restaurants.Thai:
     print("Menu in preparation.")
 
-elif choose_res == '4':
+elif choose_res == Restaurants.Polish:
     print("Menu in preparation.")
 
-elif choose_res == '5':
+elif choose_res == Restaurants.Pizza:
     print("Menu in preparation.")
 
-elif choose_res == '6':
+elif choose_res == Restaurants.Kebab:
     print("Menu in preparation.")
     
 else:
